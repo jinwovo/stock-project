911ae345875fd40ee74653075a3c799c5303a0fb
jwoo99 <kownjinwoo@gmail.com>
2022-07-08 16:05:07 +0900

    프론트수정

--- react-front/src/routes.js
+++ react-front/src/routes.js
@@ -1,20 +1,4 @@
-/*!
 
-=========================================================
-* Light Bootstrap Dashboard React - v2.0.1
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/light-bootstrap-dashboard-react
-* Copyright 2022 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/light-bootstrap-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import Dashboard from "views/Dashboard.js";
 import KospiNasdaq from "views/kospiandnasdaq.js";
 import Kospi from "views/Kospi.js";
@@ -53,7 +37,7 @@ const dashboardRoutes = [
     layout: "/admin"
   },
   {
-    path: "/kospi",
+    path: "/stockofkospi",
     name: "STOCK OF KOSPI",
     image: kospimini,
     component: Kospi,
@@ -82,5 +66,9 @@ const dashboardRoutes = [
     layout: "/admin"
   }
 ];
+
+
+
+
 
 export default dashboardRoutes;
